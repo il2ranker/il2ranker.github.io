6c4832fff8fb000846e5b1a961571c413f085606
CAT <164977446+il2ranker@users.noreply.github.com>
2024-06-15 21:04:17 +0100

    finally 6 pls.

--- scripts/main.js
+++ scripts/main.js
@@ -418,7 +418,7 @@ var trainees = [];
 var filteredTrainees = [];
 // holds the ordered list of rankings that the user selects
 var ranking = newRanking();
-const rowNums = [1,2,4];
+const rowNums = [1,2,3];
 //window.addEventListener("load", function () {
   populateRanking();
   readFromCSV("./trainee_info.csv");
